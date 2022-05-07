4150534af69aa3f5180ac65346facbe7428f2f32
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-08 00:12:19 +0600

    env file setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.port || 5000;
@@ -10,7 +11,7 @@ const port = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://freshDale:freshDale12345@cluster0.vl9uy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vl9uy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
