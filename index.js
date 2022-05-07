85ca2ec4926650e6a462350be7bc847acc053749
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-08 01:04:55 +0600

    env file setup update

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vl9uy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
