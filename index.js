54455aff357445eaaa401dde0acaba7a44b34d9f
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-07 08:51:49 +0600

    server setup and first get products api make

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log(test);
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.port || 5000;
+
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://freshDale:freshDale12345@cluster0.vl9uy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("cycleZone").collection("products");
+
+        // get users
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running my server');
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
