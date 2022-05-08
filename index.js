47ab7c2e0a0efeaa3c3e7b7a319c8b5033287d13
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-08 11:01:37 +0600

    error check

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 
 // use middleware
